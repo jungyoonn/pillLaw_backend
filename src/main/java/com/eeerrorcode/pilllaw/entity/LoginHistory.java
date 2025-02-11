efda6501834f426bd430306d6e4a82479f758f34
jungyoonn <sophia7625@naver.com>
2025-02-11 18:24:36 +0900

    로그인 이력 엔티티

---
+++ src/main/java/com/eeerrorcode/pilllaw/entity/LoginHistory.java
@@ -0,0 +1,32 @@
+package com.eeerrorcode.pilllaw.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "tbl_login_history")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class LoginHistory extends BaseEntity{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long lno;
+
+  private LocalDateTime loginTime;
+
+  private String ip;
+
+  private String device;
+
+  private boolean success;
+
+  private String failureReason;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "mno")
+  private Member member;
+}
