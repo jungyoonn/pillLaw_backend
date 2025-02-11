82622a05ea86809bee65722eef26af7ac6ffa723
DahnDell <khanman1208@gmail.com>
2025-02-11 18:25:09 +0900

    Product.java entitiy 정의

---
+++ src/main/java/com/eeerrorcode/pilllaw/entity/Product.java
@@ -0,0 +1,43 @@
+package com.eeerrorcode.pilllaw.entity;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Product extends BaseEntity{
+  
+  @Id
+  private Long pno;
+  private String pname;
+  private String company;
+  private LocalDateTime bestBefore;
+  private String effect;
+  private String precautions;
+  private String keep;
+  // private Category cname;
+  private boolean state;
+  
+  @Builder.Default
+  @ElementCollection(fetch = FetchType.LAZY)
+  private Set<ProductType> typeSet = new HashSet<>();
+
+  public void addProductType(ProductType pt){
+    typeSet.add(pt);
+  }
+
+
+}
