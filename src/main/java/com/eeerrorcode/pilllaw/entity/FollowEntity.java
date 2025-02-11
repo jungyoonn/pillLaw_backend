de2095f31c12a6e8df87ee4ed5f622b3d1bc402a
mawoo <hanjutun@gmail.com>
2025-02-11 15:07:14 +0900

    entity추가

---
+++ src/main/java/com/eeerrorcode/pilllaw/entity/FollowEntity.java
@@ -0,0 +1,30 @@
+package com.eeerrorcode.pilllaw.entity;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "tbl_follow_entity")
+public class FollowEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long followId;
+    private String senderFollowId; //보낸 사람
+    private String receiverFollowId; //받는 사람
+    private LocalDate createdAt;
+    private Boolean isFollowBack;
+
+}
---
+++ src/main/java/com/eeerrorcode/pilllaw/entity/LetterEntity.java
@@ -0,0 +1,33 @@
+package com.eeerrorcode.pilllaw.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+// @Getter
+// @Builder
+// @Setter
+// @AllArgsConstructor
+// @NoArgsConstructor
+public class LetterEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long letter;
+
+    private String sender;
+    private String receiver;
+    private String content;
+    private LocalDateTime sentAt;
+    
+}
