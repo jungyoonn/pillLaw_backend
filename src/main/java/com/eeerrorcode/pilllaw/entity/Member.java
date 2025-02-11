27494bc691fcc4e1c1f0e489ddb45a994958bff9
jungyoonn <sophia7625@naver.com>
2025-02-11 16:57:14 +0900

    Member Entity

---
+++ src/main/java/com/eeerrorcode/pilllaw/entity/Member.java
@@ -0,0 +1,55 @@
+package com.eeerrorcode.pilllaw.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "tbl_member")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Member extends BaseEntity{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long mno;
+
+  private String email;
+
+  private String password;
+
+  private String name;
+
+  private String nickname;
+
+  private String tel;
+
+  private boolean firstLogin;
+
+  @Builder.Default
+  @ElementCollection(fetch = FetchType.LAZY)
+  private Set<MemberRole> roleSet = new HashSet<>();
+
+  public void addMemberRole(MemberRole memberRole) {
+    roleSet.add(memberRole);
+  }
+
+  @Builder.Default
+  @ElementCollection(fetch = FetchType.LAZY)
+  private Set<MemberStatus> statusSet = new HashSet<>();
+
+  public void addMemberStatus(MemberStatus memberStatus) {
+    statusSet.add(memberStatus);
+  }
+
+  @Builder.Default
+  @ElementCollection(fetch = FetchType.LAZY)
+  private Set<MemberAccount> accountSet = new HashSet<>();
+
+  public void addMemberAccount(MemberAccount memberAccount) {
+    accountSet.add(memberAccount);
+  }
+}
