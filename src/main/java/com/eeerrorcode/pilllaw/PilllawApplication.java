db15eb28615995d9a9969dfc95a4319a941fc867
jungyoonn <sophia7625@naver.com>
2025-02-11 12:07:32 +0900

    프로젝트 폴더 변경 후 환경  재설정

---
+++ src/main/java/com/eeerrorcode/pilllaw/PilllawApplication.java
@@ -0,0 +1,13 @@
+package com.eeerrorcode.pilllaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilllawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilllawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eeerrorcode/pilllaw/PilllawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeerrorcode.pilllaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilllawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
