222b090681f6ec1e87ae8728818bfba8a390585d
jungyoonn <sophia7625@naver.com>
2025-02-11 18:24:44 +0900

    배송지 목록 엔티티

---
+++ src/main/java/com/eeerrorcode/pilllaw/entity/MemberAddress.java
@@ -0,0 +1,32 @@
+package com.eeerrorcode.pilllaw.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "tbl_member_address")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class MemberAddress extends BaseEntity{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long addrno;
+
+  private String recipient;
+
+  private String tel;
+
+  private String postalCode;
+
+  private String readAddress;
+
+  private String detailAddress;
+
+  private boolean defaultAddr;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "mno")
+  private Member member;
+}
