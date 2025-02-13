d6bb54c155e330220fd0b2dd1864e045eff1a902
jungyoonn <sophia7625@naver.com>
2025-02-13 15:27:05 +0900

    + findByEmailAndAccountSet

--- src/main/java/com/eeerrorcode/pilllaw/repository/MemberRepository.java
+++ src/main/java/com/eeerrorcode/pilllaw/repository/MemberRepository.java
@@ -1,12 +1,15 @@
 package com.eeerrorcode.pilllaw.repository;
 
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eeerrorcode.pilllaw.entity.member.Member;
+import com.eeerrorcode.pilllaw.entity.member.MemberAccount;
 
 
 public interface MemberRepository  extends JpaRepository<Member, Long>{
   Optional<Member> findByEmail(String email);
+  Optional<Member> findByEmailAndAccountSet(String email, Set<MemberAccount> accounts);
 }
