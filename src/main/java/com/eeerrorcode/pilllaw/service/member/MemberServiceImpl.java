753790e45b3575096762d462a2938d637e82527a
jungyoonn <sophia7625@naver.com>
2025-02-13 18:00:34 +0900

    메일 발송 관련 메서드 추가

--- src/main/java/com/eeerrorcode/pilllaw/service/member/MemberServiceImpl.java
+++ src/main/java/com/eeerrorcode/pilllaw/service/member/MemberServiceImpl.java
@@ -1,12 +1,17 @@
 package com.eeerrorcode.pilllaw.service.member;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.eeerrorcode.pilllaw.dto.member.EmailDto;
 import com.eeerrorcode.pilllaw.dto.member.MemberDto;
 import com.eeerrorcode.pilllaw.entity.member.LoginResult;
 import com.eeerrorcode.pilllaw.repository.MemberRepository;
@@ -22,6 +27,11 @@ public class MemberServiceImpl implements MemberService{
   private MemberRepository repository;
   @Autowired
   private PasswordEncoder encoder;
+  @Autowired
+  private MailService mailService;
+
+  @Value("${spring.mail.auth-code-expiration-millis}")
+  private long authCodeExpirationMillis;
 
   @Override
   public Optional<MemberDto> get(Long mno) {
@@ -70,6 +80,54 @@ public class MemberServiceImpl implements MemberService{
     }
 
     return LoginResult.SUCCESS;
+  }
+
+  @Override
+  public EmailDto sendCodeToEmail(String email) {
+    if(checkDuplicatedEmail(email)) {
+      return null;
+    }
+
+    String title = "Pill Law 인증번호 발송";
+    String authCode = this.createCode();
+
+    mailService.sendEmail(email, title, authCode);
+
+    EmailDto dto = EmailDto.builder().email(email).authCode(authCode).build();
+
+    return dto;
+  }
+
+  @Override
+  public boolean checkDuplicatedEmail(String email) {
+    Optional<MemberDto> optional = toOptionalDto(repository.findByEmail(email).orElse(null));  
+    if(optional.isEmpty()) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String createCode() {
+    int length = 6;
+    Random random;
+    StringBuilder builder = new StringBuilder();
+
+    try {
+      random = SecureRandom.getInstanceStrong();
+      for (int i = 0; i < length; i++) {
+        builder.append(random.nextInt(10));
+      }
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+    
+    return builder.toString();
+  }
+
+  @Override
+  public boolean verifiedCode(String email, String inputCode) {
+    return false;
   }
   
 }
