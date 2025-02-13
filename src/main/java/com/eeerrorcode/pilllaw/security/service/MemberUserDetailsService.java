84e88a22e9a56c7a626dacc7bec9345df1ddc0ef
jungyoonn <sophia7625@naver.com>
2025-02-13 15:26:47 +0900

    Auth service

---
+++ src/main/java/com/eeerrorcode/pilllaw/security/service/MemberUserDetailsService.java
@@ -0,0 +1,49 @@
+package com.eeerrorcode.pilllaw.security.service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.eeerrorcode.pilllaw.dto.member.MemberDto;
+import com.eeerrorcode.pilllaw.entity.member.MemberAccount;
+import com.eeerrorcode.pilllaw.repository.MemberRepository;
+import com.eeerrorcode.pilllaw.security.dto.AuthMemberDto;
+import com.eeerrorcode.pilllaw.service.member.MemberService;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class MemberUserDetailsService implements UserDetailsService{
+  @Autowired
+  private MemberRepository repository;
+
+  private MemberService memberService;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<MemberDto> optional = memberService.toOptionalDto(repository.findByEmailAndAccountSet(username
+    , new HashSet<>(Set.of(MemberAccount.NORMAL))).orElse(null));
+  
+    if(optional.isEmpty()) {
+      throw new UsernameNotFoundException(username);
+    }
+    MemberDto dto = optional.get();
+
+    AuthMemberDto authMemberDto = new AuthMemberDto(dto.getEmail(), dto.getPassword(), dto.getMno(), dto.getAccounts()
+    , dto.getName(), dto.getNickname(), dto.getTel(), dto.isFirstLogin()
+    , dto.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.name())).toList());
+
+    return authMemberDto;
+  }
+  
+}
