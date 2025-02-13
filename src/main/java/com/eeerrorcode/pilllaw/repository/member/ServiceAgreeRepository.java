4000d8ca8f5f070b53b4d94a7fa58bd3329f7b0c
jungyoonn <sophia7625@naver.com>
2025-02-13 17:51:11 +0900

    약관 동의 repository

---
+++ src/main/java/com/eeerrorcode/pilllaw/repository/member/ServiceAgreeRepository.java
@@ -0,0 +1,9 @@
+package com.eeerrorcode.pilllaw.repository.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eeerrorcode.pilllaw.entity.member.ServiceAgree;
+
+public interface ServiceAgreeRepository extends JpaRepository<ServiceAgree, Long>{
+  
+}
