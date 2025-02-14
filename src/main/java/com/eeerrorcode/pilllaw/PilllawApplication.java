fb8532bd9e20d0a760ffcbd9e3169ba84b5d603a
jungyoonn <sophia7625@naver.com>
2025-02-14 14:10:00 +0900

    어노테이션 설정 제거

--- src/main/java/com/eeerrorcode/pilllaw/PilllawApplication.java
+++ src/main/java/com/eeerrorcode/pilllaw/PilllawApplication.java
@@ -2,10 +2,9 @@ package com.eeerrorcode.pilllaw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @EnableJpaAuditing // regDate, modDate 적용 어노테이션
 public class PilllawApplication {
 
