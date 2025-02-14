7a829c19d7b56281d2154cb448a303f627d7aec3
mekwon92 <me92100984@gmail.com>
2025-02-14 13:34:22 +0900

    orderitem import 수정

--- src/main/java/com/eeerrorcode/pilllaw/entity/order/OrderItem.java
+++ src/main/java/com/eeerrorcode/pilllaw/entity/order/OrderItem.java
@@ -10,8 +10,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity(name = "tbl_order_item")
 @Getter
