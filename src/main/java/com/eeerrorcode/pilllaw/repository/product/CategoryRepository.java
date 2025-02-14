7a5b3f61275b836cd71d8001fac093bfc632d761
DahnDell <khanman1208@gmail.com>
2025-02-14 11:26:47 +0900

    CategoryRepository 카테고리 조회메서드 추가

--- src/main/java/com/eeerrorcode/pilllaw/repository/product/CategoryRepository.java
+++ src/main/java/com/eeerrorcode/pilllaw/repository/product/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.eeerrorcode.pilllaw.repository.product;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.eeerrorcode.pilllaw.entity.product.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>{
-  
+  Optional<Category> findByCname(String cname);
 }
