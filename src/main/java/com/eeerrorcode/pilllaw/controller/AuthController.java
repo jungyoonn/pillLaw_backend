9f3b355c09bc18ab3e3453a94c0aca4bb6340922
jungyoonn <sophia7625@naver.com>
2025-02-14 14:09:44 +0900

    로그인 틀

--- src/main/java/com/eeerrorcode/pilllaw/controller/AuthController.java
+++ src/main/java/com/eeerrorcode/pilllaw/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +17,20 @@ import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
-@RequestMapping("/member")
+@RequestMapping("/api")
 public class AuthController {
   @Autowired
   private MemberService service;
+
+  @RequestMapping(value = "/signin", method = RequestMethod.OPTIONS)
+  public ResponseEntity<?> handleOptions() {
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/signin")
+  public ResponseEntity<?> signin(@RequestParam String email) {
+    return ResponseEntity.ok("로그인 성공: " + email);
+  }
 
   @PostMapping("/email/verification-requests")
   public ResponseEntity<?> sendMail(@RequestParam("email") @Valid String email) {
