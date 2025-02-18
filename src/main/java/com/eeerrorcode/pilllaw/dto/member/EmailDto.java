e08e36144143aa21ef95fd80e80497a2fd992a3b
jungyoonn <sophia7625@naver.com>
2025-02-18 17:11:39 +0900

    EmailDto 개량

--- src/main/java/com/eeerrorcode/pilllaw/dto/member/EmailDto.java
+++ src/main/java/com/eeerrorcode/pilllaw/dto/member/EmailDto.java
@@ -1,5 +1,9 @@
 package com.eeerrorcode.pilllaw.dto.member;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import lombok.*;
 
 @Getter
@@ -9,6 +13,20 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 public class EmailDto {
+  private Long id;
   private String email;
-  private String authCode;
+  private String token;
+  private Instant expiresAt;
+  private LocalDateTime regDate;
+  private LocalDateTime modDate;
+
+  public EmailDto(String email, long expirationMillis) {
+    this.email = email;
+    this.token = UUID.randomUUID().toString(); // 랜덤한 고유 토큰 생성
+    this.expiresAt = Instant.now().plusMillis(expirationMillis); // 만료 시간 설정
+  }
+
+  public boolean isExpired() {
+    return Instant.now().isAfter(expiresAt);
+  }
 }
