9fb7693df772afe8f22ab6ad71dddd9525f57512
jungyoonn <sophia7625@naver.com>
2025-02-19 12:06:53 +0900

    메인 컨트롤러 생성, 사용자 정보 프론트에 전달

---
+++ src/main/java/com/eeerrorcode/pilllaw/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.eeerrorcode.pilllaw.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.eeerrorcode.pilllaw.service.member.MemberService;
+
+import lombok.extern.log4j.Log4j2;
+
+
+@RestController
+@RequestMapping("/api")
+@Log4j2
+public class MainController {
+  @Autowired
+  private MemberService memberService;
+
+  @GetMapping("")
+  public ResponseEntity<?> index(@RequestParam String email) {
+    log.info("email => {}", email);
+    log.info("findByEmail => {}", memberService.getByEmail(email));
+    return ResponseEntity.ok(memberService.getByEmail(email));
+  }
+  
+}
