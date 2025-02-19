1211aa43e44c3201a355db956e9ebf5b65671b57
DahnDell <khanman1208@gmail.com>
2025-02-19 20:17:34 +0900

    Repository 메서드 추가

--- src/main/java/com/eeerrorcode/pilllaw/repository/product/ProductDetailRepository.java
+++ src/main/java/com/eeerrorcode/pilllaw/repository/product/ProductDetailRepository.java
@@ -1,11 +1,18 @@
 package com.eeerrorcode.pilllaw.repository.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.eeerrorcode.pilllaw.entity.product.ProductDetail;
+import java.util.Optional;
+
 
 @Repository
 public interface ProductDetailRepository extends JpaRepository<ProductDetail, Long>{
+  
+  @Query("SELECT pd FROM com.eeerrorcode.pilllaw.entity.product.ProductDetail pd WHERE pd.product.pno = :pno")
+  Optional<ProductDetail> findByProduct(@Param("pno") Long pno);
   
 }
