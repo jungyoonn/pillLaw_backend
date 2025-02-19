eec3c00f6e8f7826fe34a019bd19c05038f52f3a
DahnDell <khanman1208@gmail.com>
2025-02-19 20:17:47 +0900

    Repository 메서드 수정

--- src/main/java/com/eeerrorcode/pilllaw/repository/product/ProductRepository.java
+++ src/main/java/com/eeerrorcode/pilllaw/repository/product/ProductRepository.java
@@ -1,11 +1,9 @@
 package com.eeerrorcode.pilllaw.repository.product;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.eeerrorcode.pilllaw.entity.product.CategoryType;
 import com.eeerrorcode.pilllaw.entity.product.Product;
 
 @Repository
