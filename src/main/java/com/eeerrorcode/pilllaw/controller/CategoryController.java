27f0d0908e98af58ce1ad910f69382cb01b0ec0f
DahnDell <khanman1208@gmail.com>
2025-02-20 18:26:42 +0900

    CategoryController 수정

--- src/main/java/com/eeerrorcode/pilllaw/controller/CategoryController.java
+++ src/main/java/com/eeerrorcode/pilllaw/controller/CategoryController.java
@@ -2,23 +2,38 @@ package com.eeerrorcode.pilllaw.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eeerrorcode.pilllaw.entity.product.CategoryType;
 import com.eeerrorcode.pilllaw.service.product.CategoryService;
 
 import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
 @Log4j2
+@RequestMapping("api/v1/category")
 public class CategoryController {
 
+  @Autowired
   private CategoryService categoryService;
   
 
-  @GetMapping("path")
-  public String getMethodName(@RequestParam String param) {
-      return new String();
+  @GetMapping("/list/bioactive")
+  public ResponseEntity<List<?>> getBioActivity() {
+    List<?> categoryTypes = categoryService.listCategoryByType(CategoryType.BIOACTIVE);
+    return ResponseEntity.ok(categoryTypes);
+  }
+
+  @GetMapping("/list/nutrient")
+  public ResponseEntity<List<?>> getNutrient() {
+    List<?> categoryTypes = categoryService.listCategoryByType(CategoryType.NUTRIENT);
+    return ResponseEntity.ok(categoryTypes);
   }
   
 }
