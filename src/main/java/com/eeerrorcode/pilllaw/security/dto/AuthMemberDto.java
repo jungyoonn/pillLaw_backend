ed7807d57df98adabaf714488733a58c0ec95570
jungyoonn <sophia7625@naver.com>
2025-02-21 17:55:35 +0900

    OAuth2 호출 생성자 추가

--- src/main/java/com/eeerrorcode/pilllaw/security/dto/AuthMemberDto.java
+++ src/main/java/com/eeerrorcode/pilllaw/security/dto/AuthMemberDto.java
@@ -3,10 +3,13 @@ package com.eeerrorcode.pilllaw.security.dto;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import com.eeerrorcode.pilllaw.dto.member.SocialMemberDto;
 import com.eeerrorcode.pilllaw.entity.member.MemberAccount;
 import com.eeerrorcode.pilllaw.entity.member.MemberRole;
 import com.eeerrorcode.pilllaw.entity.member.MemberStatus;
@@ -16,7 +19,7 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
-public class AuthMemberDto extends User {
+public class AuthMemberDto extends User implements OAuth2User{
   private Long mno;
   private String email;
   private String password;
@@ -24,6 +27,8 @@ public class AuthMemberDto extends User {
   private String nickname;
   private String tel;
   private Boolean firstLogin;
+  private Map<String, Object> attr;
+  private SocialMemberDto dto;
 
   private List<MemberRole> roles = new ArrayList<>();
 
@@ -46,5 +51,29 @@ public class AuthMemberDto extends User {
     this.roles = roles;
     this.status = status;
     this.password = password;
+  }
+
+  // OAuth2 호출 생성자
+  public AuthMemberDto(String username, String password, Long mno, List<MemberAccount> accounts, List<MemberRole> roles
+  ,List<MemberStatus> status, String name, String nickname, String tel, boolean firstLogin
+  ,Collection<? extends GrantedAuthority> authorities, Map<String, Object> attr, SocialMemberDto dto) {
+    super(username, password != null ? password : "SOCIAL_LOGIN_USER", authorities);
+    this.name = name;
+    this.mno = mno;
+    this.nickname = nickname;
+    this.tel = tel;
+    this.firstLogin = firstLogin;
+    this.email = username;
+    this.accounts = accounts;
+    this.roles = roles;
+    this.status = status;
+    this.password = password;
+    this.attr = attr;
+    this.dto = dto;
+  }
+
+  @Override
+  public Map<String, Object> getAttributes() {
+    return attr;
   }
 }
