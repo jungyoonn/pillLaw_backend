38646ad70409a74188fee1855180341f3b6536cb
jungyoonn <sophia7625@naver.com>
2025-02-21 17:55:50 +0900

    소셜 로그인 성공 핸들러

--- src/main/java/com/eeerrorcode/pilllaw/security/handler/LoginSuccessHandler.java
+++ src/main/java/com/eeerrorcode/pilllaw/security/handler/LoginSuccessHandler.java
@@ -1,17 +1,14 @@
 package com.eeerrorcode.pilllaw.security.handler;
 
 import java.io.IOException;
-import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import com.eeerrorcode.pilllaw.entity.member.MemberAccount;
-import com.eeerrorcode.pilllaw.security.dto.AuthMemberDto;
-import com.eeerrorcode.pilllaw.security.filter.CustomAuthenticationToken;
+import com.eeerrorcode.pilllaw.security.util.JWTUtil;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,14 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-public class LoginSuccessHandler implements AuthenticationSuccessHandler{
-  private RedirectStrategy strategy = new DefaultRedirectStrategy();
-  
-  private PasswordEncoder encoder;
-  
-  public LoginSuccessHandler(PasswordEncoder encoder) {
-    this.encoder = encoder;
-  }
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+  @Autowired
+  private JWTUtil jwtUtil;
   
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -34,15 +26,42 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler{
     log.info("::::::::::::::");
     log.info("on authentication success!");
 
-    if (authentication instanceof CustomAuthenticationToken customAuth) {
-      String rawPassword = customAuth.getRawPassword();
-      
-      AuthMemberDto authMember = (AuthMemberDto) authentication.getPrincipal();
-      List<MemberAccount> accounts = authMember.getAccounts();
-      boolean passwordResult = encoder.matches(rawPassword, authMember.getPassword());
+    OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+    String email = oAuth2User.getAttribute("email");
+    String token = jwtUtil.generateToken(email);
+    String targetUrl = request.getParameter("redirect_uri");
 
-      log.info("비밀번호 일치 여부: " + passwordResult);
+    if (targetUrl == null || targetUrl.isEmpty()) {
+      targetUrl = "http://localhost:3000/pilllaw/oauth2/redirect";  // 기본 리다이렉트 URL
     }
+
+    // 리다이렉트 URI에 토큰과 이메일 추가
+    String finalUrl = UriComponentsBuilder.fromUriString(targetUrl)
+      .queryParam("token", token)
+      .queryParam("email", email)
+      .build().toUriString();
+
+    // 리다이렉트
+    getRedirectStrategy().sendRedirect(request, response, finalUrl);
+
+    // if (authentication instanceof CustomAuthenticationToken customAuth) {
+    //   // String rawPassword = customAuth.getRawPassword();
+      
+    //   AuthMemberDto authMember = (AuthMemberDto) authentication.getPrincipal();
+    //   List<MemberAccount> accounts = authMember.getAccounts();
+    //   // boolean passwordResult = encoder.matches(rawPassword, authMember.getPassword());
+    //   // log.info("비밀번호 일치 여부: " + passwordResult);
+
+    //   // strategy.sendRedirect(request, response,  "/");
+    // }
   }
   
+  @Override
+  protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+    String targetUrl = request.getParameter("redirect_uri");
+    if (targetUrl != null && !targetUrl.isEmpty()) {
+      return targetUrl;
+    }
+    return super.determineTargetUrl(request, response, authentication);
+  }
 }
