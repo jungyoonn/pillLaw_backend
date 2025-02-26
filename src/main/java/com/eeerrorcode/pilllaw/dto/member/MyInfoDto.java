42f217dd03d508b99104e7e8658d91aa86190238
jungyoonn <sophia7625@naver.com>
2025-02-26 11:47:19 +0900

    마이페이지 - 내 정보 탭에 필요한 dto

---
+++ src/main/java/com/eeerrorcode/pilllaw/dto/member/MyInfoDto.java
@@ -0,0 +1,16 @@
+package com.eeerrorcode.pilllaw.dto.member;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MyInfoDto {
+  private MemberDto memberDto;
+  private SocialMemberDto socialDto;
+  private Integer follower;
+  private Integer following;
+}
