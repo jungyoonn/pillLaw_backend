66177b8aaacd9a7cc82940a950be40809c712ba0
mawoo <hanjutun@gmail.com>
2025-02-26 12:26:01 +0900

    팔로우count Dto,Controller 추가

---
+++ src/main/java/com/eeerrorcode/pilllaw/controller/FollowCountController.java
@@ -0,0 +1,42 @@
+package com.eeerrorcode.pilllaw.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eeerrorcode.pilllaw.dto.follow.FollowCountDto;
+import com.eeerrorcode.pilllaw.entity.follow.Follow;
+import com.eeerrorcode.pilllaw.repository.follow.FollowRepository;
+import com.eeerrorcode.pilllaw.service.follow.FollowService;
+import com.eeerrorcode.pilllaw.service.member.MemberService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/follow")
+public class FollowCountController {
+
+  @Autowired
+  private FollowService followService;
+  @Autowired
+  private FollowRepository repository;
+
+  @GetMapping("/count/{mno}")
+  public ResponseEntity<?> getCount(@PathVariable("mno") String mno) {
+    if (mno == null) {
+      return ResponseEntity.notFound().build();
+    }
+    Long reqMno = Long.valueOf(mno);
+
+    FollowCountDto countDto = new FollowCountDto();
+    countDto.setFollower(followService.getSender_Mno(Long.valueOf(reqMno)).size());
+    countDto.setFollowing(followService.getReceiver_Mno(Long.valueOf(reqMno)).size());
+    return ResponseEntity.ok(countDto);
+  }
+}
---
+++ src/main/java/com/eeerrorcode/pilllaw/dto/follow/FollowCountDto.java
@@ -0,0 +1,18 @@
+package com.eeerrorcode.pilllaw.dto.follow;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FollowCountDto {
+  private Integer follower;
+  private Integer following;
+
+}
