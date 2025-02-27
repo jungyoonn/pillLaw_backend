2047bbab6525931bb2c952c5a2f898112a7139e1
jungyoonn <sophia7625@naver.com>
2025-02-27 15:22:57 +0900

    기본 배송지 가져오기, 배송지 목록 가져오기 JPQL 추가

--- src/main/java/com/eeerrorcode/pilllaw/repository/member/MemberAddressRepository.java
+++ src/main/java/com/eeerrorcode/pilllaw/repository/member/MemberAddressRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.eeerrorcode.pilllaw.entity.member.MemberAddress;
 
@@ -17,4 +19,9 @@ public interface MemberAddressRepository extends JpaRepository<MemberAddress, Lo
   // findByMember_IdAndPostalCodeAndRoadAddressAndDetailAddressAndTel(Long mno,
   // String postalCode, String roadAddress, String detailAddress, String tel);
 
+  @Query("SELECT a FROM tbl_member_address a WHERE a.member.mno = :mno AND a.defaultAddr = :defaultAddr")
+  Optional<MemberAddress> findByMnoAndDefaultAddr(@Param("mno") Long mno, @Param("defaultAddr") boolean defaultAddr);
+
+  @Query("SELECT a FROM tbl_member_address a WHERE a.member.mno = :mno")
+  List<MemberAddress> findByMno(@Param("mno") Long mno);
 }
