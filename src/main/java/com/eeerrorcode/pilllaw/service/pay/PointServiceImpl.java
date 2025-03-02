99bf92882578f821d1ed99808b76633053588ee4
mekwon92 <me92100984@gmail.com>
2025-03-03 00:37:59 +0900

    포인트 00:30시에 자동적립

--- src/main/java/com/eeerrorcode/pilllaw/service/pay/PointServiceImpl.java
+++ src/main/java/com/eeerrorcode/pilllaw/service/pay/PointServiceImpl.java
@@ -55,7 +55,7 @@ public class PointServiceImpl implements PointService {
         return toDto(point); // PointDto로 반환
     }
 
-    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행
+    @Scheduled(cron = "0 30 0 * * ?") // 매일 오전 00시 30분에 실행
     @Transactional
     public int addPointsForCompletedPayments() {
         log.info("포인트 적립 스케줄 작업이 시작되었습니다."); // 스케줄 실행 시작 로그 추가
@@ -64,24 +64,27 @@ public class PointServiceImpl implements PointService {
 
         for (Pay payment : successfulPayments) {
             LocalDateTime paymentDate = payment.getRegDate();
-            LocalDateTime oneDayLater = paymentDate.plusDays(2);
 
+            // 결제일에 2일을 더한 후, 자정(00시)에 30분을 더한 시간 계산
+            LocalDateTime oneDayLater = paymentDate.plusDays(2).toLocalDate().atStartOfDay().plusMinutes(30);
+
+            // 포인트 적립 조건: 현재 시간이 'oneDayLater'보다 크거나 같으면 포인트 적립
             if (LocalDateTime.now().isAfter(oneDayLater)) {
                 Member member = payment.getOrder().getMember();
                 double pointRatio = 0.02;
                 long pointsToAdd = (long) (payment.getTotalPrice() * pointRatio);
 
-                
                 Point point = Point.builder()
-                .member(member)
-                .point(pointsToAdd)
-                .status(PointStatus.EARNED)
-                .endDate(LocalDateTime.now().plusYears(1))
-                .build();
-                
+                        .member(member)
+                        .point(pointsToAdd)
+                        .status(PointStatus.EARNED)
+                        .endDate(LocalDateTime.now().plusYears(1))
+                        .build();
+
                 pointRepository.save(point);
 
-                log.info("회원 mno: {}, 결제일: {}, oneDayLater: {}, 현재시간: {}", member.getMno(), paymentDate, oneDayLater, LocalDateTime.now());
+                log.info("회원 mno: {}, 결제일: {}, oneDayLater: {}, 현재시간: {}", member.getMno(), paymentDate, oneDayLater,
+                        LocalDateTime.now());
 
                 pointCount++;
             }
